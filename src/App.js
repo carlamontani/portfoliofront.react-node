6a06f74308debe06147a2108e7d620ea2108ac67
Carla Montani <carlamontani@gmail.com>
2020-08-12 01:07:43 -0300

    correciones header

--- src/App.js
+++ src/App.js
@@ -10,14 +10,16 @@ import Education from './Component/Education/Education';
 function App() {
   return (
     <div className="App">
-      <div className="Links">
-        <p className="icons no-margin">
-          <a href="https://www.linkedin.com/in/carla-montani-7b3231112" add target="_blank"><i class="fab fa-linkedin-in icon"></i></a>
-          <a href="https://github.com/carlamontani" add target="_blank"><i class="fab fa-github icon"></i></a>
-          <a href="https://www.behance.net/carlamontani" add target="_blank"><i class="fab fa-behance icon"></i></a>
-        </p>
+      <div className="Header">
+        <div className="Links">
+          <p className="icons no-margin">
+            <a href="https://www.linkedin.com/in/carla-montani-7b3231112" add target="_blank"><i class="fab fa-linkedin-in icon"></i></a>
+            <a href="https://github.com/carlamontani" add target="_blank"><i class="fab fa-github icon"></i></a>
+            <a href="https://www.behance.net/carlamontani" add target="_blank"><i class="fab fa-behance icon"></i></a>
+          </p>
+        </div>
+        <About/>
       </div>
-      <About/>
       <Education/>
       <Portfolio/>
       <Skills/>
